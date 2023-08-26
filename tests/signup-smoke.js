e7fa3ec5085064df7ed949f2de1c23f5f9890f52
silva1996 <marcosj.antonio3@gmail.com>
2023-08-25 21:04:05 -0300

    first commit

---
+++ tests/signup-smoke.js
@@ -0,0 +1,35 @@
+import http from 'k6/http'
+import { sleep, check } from 'k6'
+
+import uuid from './libs/uuid.js'
+
+export const options = {
+  vus:1,
+  duration: '1m',
+  thresholds: {
+    http_req_duration: ['p(95)<2000'],
+    http_req_failed: ['rate<0.01']
+  }
+}
+
+export default function () {
+const url = 'http://localhost:3333/signup'
+
+  const payload = JSON.stringify({ email: `${uuid.v4().substring(24)}@qa.qacademy.com.br` , password: 'pwd123' })
+
+  const headers = {
+    'headers': {
+      'Content-Type': 'application/json'
+    }
+  }
+  
+  const res = http.post(url, payload, headers)
+
+  console.log(res.body)
+
+  check(res, {
+    'status should be 201': (r) => r.status === 201
+  })  
+
+   sleep(1)
+}
